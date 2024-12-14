1c10640099ef63bd9f3b54601e2e205f9c8f28a6
Chetan Morey <146196075+ChetanMorey44@users.noreply.github.com>
2024-12-14 16:29:16 +0530

    Add files via upload

---
+++ ElseIf.java
@@ -0,0 +1,28 @@
+//package Day04;
+//
+//import java.util.Scanner;
+//
+//public class ElseIf {
+//     
+//	public static void main(String[] args) {
+//	
+////		int number = 50;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter a Number!! ");
+//		int number = sc.nextInt();
+//		
+//		if(number >= 56) {
+//			System.out.println("Fast");
+//		}else if(number >= 56 && number >= 56 ) {
+//			System.out.println("Second");
+//		}else if(number >= 56 && number <= 56) {
+//			System.out.println("Done");
+//		}else {
+//			System.out.println("end");
+//		}
+//		
+//	}
+//}
+//
+
+
